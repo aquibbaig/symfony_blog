ce21bed8d47b99dd6bf56c9c1009e42d3d6be9fc
Aquib Baig <aquibbaig97@gmail.com>
2019-03-28 13:37:05 +0530

    add file changes and push to production

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
