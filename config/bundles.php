3091a68d6c1832eac0ac8288f534968840ab433f
Aquib Baig <aquibbaig97@gmail.com>
2019-03-28 23:19:50 +0530

    fixed class not found and some tweaks

--- config/bundles.php
+++ config/bundles.php
@@ -16,5 +16,4 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true]
 ];
